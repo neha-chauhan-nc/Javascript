3b8a38c327ec938d1c08b4034933bb1ec03d97e7
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-07-24 18:31:01 +0000

    modiffied

--- DAY1/Printno.js
+++ DAY1/Printno.js
@@ -1,6 +1,18 @@
 
-let num= 5;
+let num= parseInt(prompt("Enter a no:-"));
  for(i=1;i<=num;i++){
     console.log(i);
 }
+
+
+//Q2.
+
+let nums=parseInt(prompt("Enter a number:- "))
+
+for(i=nums; i>=1;i--){
+    console.log(nums);
+}
+
+
+
 
