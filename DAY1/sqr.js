45c9753128571b4d72c4d55563ba3b17c7a7c4f7
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-07-27 18:27:46 +0000

    sqr

---
+++ DAY1/sqr.js
@@ -0,0 +1,5 @@
+// Program to calculate square of a number
+let num = prompt("Enter a number:");  // user se input
+num = Number(num);                    // string ko number mein convert
+let square = num * num;               // square calculate
+alert("Square of " + num + " is " + square);
