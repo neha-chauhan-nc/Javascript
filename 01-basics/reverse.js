f54f00fd690f0ca7f98d0b4206e2d226df31419a
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-07-28 17:56:52 +0000

    reverse

---
+++ 01-basics/reverse.js
@@ -0,0 +1,8 @@
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+let reversed = [];
+
+for (let i = fruits.length - 1; i >= 0; i--) {
+  reversed.push(fruits[i]);
+}
+
+console.log("Reversed array:", reversed);
