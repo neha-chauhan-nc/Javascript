70145b0b4393173cdd044ff81c3c5e15c13eb62f
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-07-29 18:08:55 +0000

    compare

---
+++ 01-basics/compare.js
@@ -0,0 +1,9 @@
+let str1 = "apple";
+let str2 = "banana";
+
+if (str1 === str2) {
+  console.log("Strings are equal");
+} else {
+  console.log("Strings are different");
+}
+// Output: Strings are different
