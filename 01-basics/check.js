1892257d3efa4d0f9ed7cbca29095d3e5f875a9d
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-07-30 18:07:53 +0000

    check

---
+++ 01-basics/check.js
@@ -0,0 +1,9 @@
+function areAnagrams(str1, str2) {
+  let normalize = s => s.toLowerCase().split("").sort().join("");
+  return normalize(str1) === normalize(str2);
+}
+
+console.log(areAnagrams("listen", "silent")); 
+// Output: true
+console.log(areAnagrams("hello", "world")); 
+// Output: false
