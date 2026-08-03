cb473f55f9347f9a37bc0b832f2b7f2a97a8ac73
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-08-03 18:28:07 +0000

    join

---
+++ 01-basics/merges.js
@@ -0,0 +1,14 @@
+function mergeStrings(str1, str2) {
+  let result = "";
+  let maxLength = Math.max(str1.length, str2.length);
+
+  for (let i = 0; i < maxLength; i++) {
+    if (i < str1.length) result += str1[i];
+    if (i < str2.length) result += str2[i];
+  }
+
+  return result;
+}
+
+console.log(mergeStrings("abc", "123")); 
+// Output: a1b2c3
