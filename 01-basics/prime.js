f68cd11802c0bc36ffa1cf59b8fb09f45b7853e3
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-08-05 18:08:09 +0000

    prime

---
+++ 01-basics/prime.js
@@ -0,0 +1,11 @@
+// Check if a number is prime
+function isPrime(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+console.log(isPrime(7));  // Output: true
+console.log(isPrime(10)); // Output: false
