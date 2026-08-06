2fdf878936d9de40664bd1142063e511b66b84fd
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-08-06 15:49:57 +0000

    oddeven

---
+++ 01-basics/oddeven.js
@@ -0,0 +1,11 @@
+// Check if a number is even or odd
+function checkEvenOdd(num) {
+  if (num % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+console.log(checkEvenOdd(4)); // Output: Even
+console.log(checkEvenOdd(7)); // Output: Odd
