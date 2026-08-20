732984678a27271f75fe04853a91364638015cfa
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-08-20 17:56:47 +0000

    generate random password

---
+++ 01-basics/generatepassword.js
@@ -0,0 +1,12 @@
+// Generate random password of given length
+function generatePassword(length) {
+  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return password;
+}
+
+console.log(generatePassword(8));  
+// Example Output: "aB3@xY9!"
