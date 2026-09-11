341ac964fd0a284b0f7181d3624a369b22c68669
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-09-11 18:11:24 +0000

    min number

---
+++ 01-basics/minno.js
@@ -0,0 +1,13 @@
+function findMin(arr) {
+    let min = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    return min;
+}
+
+console.log(findMin([10, 4, 8, 2, 15, 6]));
