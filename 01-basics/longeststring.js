d81307db475ad7bfe7dc6dbf1385e43e669c1b3e
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-09-20 18:09:00 +0000

    longest string

---
+++ 01-basics/longeststring.js
@@ -0,0 +1,15 @@
+function longestWord(str) {
+    const words = str.split(" ");
+    let longest = "";
+
+    for (let word of words) {
+        if (word.length > longest.length) {
+            longest = word;
+        }
+    }
+
+    return longest;
+}
+
+console.log(longestWord("I love JavaScript programming"));
+// programming
